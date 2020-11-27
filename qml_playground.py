fba039f8d6cab0d0cb8cfca5b772b236595930e1
Andrey Soplevenko <soplevenko@gmail.com>
2020-11-27 17:44:15 +0300

    Python QML playground louncher created

---
+++ qml_playground.py
@@ -0,0 +1,15 @@
+import sys
+
+from PySide2.QtGui import QGuiApplication
+from PySide2.QtQml import QQmlApplicationEngine
+
+if __name__ == '__main__':
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+
+    engine.load(QUrl("main.qml"))
+    
+    if not engine.rootObjects():
+        sys.exit(-1)    
+    
+    sys.exit(app.exec_())
